2feb0e29481a007d93dac8d8c337604daf2b42c3
Md Mofazzal Hossain <mdmofazzalhossain789@gmail.com>
2025-09-05 23:38:18 +0600

    added dummy hero section

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/shared/Hero";
 import MainSection from "@/components/shared/MainSection";
 
 const page = () => {
   return (
     <div className="w-full h-full flex flex-col gap-y-2">
+      <Hero />
       <MainSection>Home Page</MainSection>
     </div>
   );
