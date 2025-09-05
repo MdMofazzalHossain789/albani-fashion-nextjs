84c88c42c7130b762bb0e45d58057afc40425b33
Md Mofazzal Hossain <mdmofazzalhossain789@gmail.com>
2025-09-06 00:36:25 +0600

    fixed grid for product section

--- components/product/ProductCardList.jsx
+++ components/product/ProductCardList.jsx
@@ -2,7 +2,7 @@ import ProductCard from "./ProductCard";
 
 const ProductCardList = () => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => (
         <ProductCard key={index} />
       ))}
