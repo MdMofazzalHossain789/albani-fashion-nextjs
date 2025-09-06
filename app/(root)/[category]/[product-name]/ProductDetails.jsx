a09092e786da0de3cfd4746dc2ac2335d5532fa1
Md Mofazzal Hossain <mdmofazzalhossain789@gmail.com>
2025-09-06 17:40:54 +0600

    fixed sizing issue

--- app/(root)/[category]/[product-name]/ProductDetails.jsx
+++ app/(root)/[category]/[product-name]/ProductDetails.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import ToggleButton from "./ToggleButton";
 import Quantity from "./Quantity";
 import Status from "./Status";
+import AddToCartButton from "@/components/product/AddToCartButton";
+import BuyNowButton from "@/components/product/BuyNowButton";
 
 const ProductDetails = () => {
   return (
@@ -11,7 +13,7 @@ const ProductDetails = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-6 lg:gap-x-20 gap-y-4 mt-4 px-4">
         {/* Image Preview */}
         <div className="flex flex-col gap-y-4 w-full">
-          <div className="relative w-full h-[70vh]">
+          <div className="relative w-full h-[55vh]">
             <Image
               src="/product.png"
               fill
@@ -22,12 +24,12 @@ const ProductDetails = () => {
 
           <div className="flex items-center gap-x-4">
             {[1, 2, 3, 4].map((item, index) => (
-              <div key={index} className="relative w-20 h-20">
+              <div key={index} className="relative w-14 h-14">
                 <Image
                   src={`/product.png`}
                   fill
                   alt="product image"
-                  className="object-cover rounded-md"
+                  className="object-cover rounded-sm"
                 />
               </div>
             ))}
@@ -35,13 +37,19 @@ const ProductDetails = () => {
         </div>
 
         {/* Product Info */}
-        <div className="w-full p-4">
+        <div className="w-full sm:p-4">
           <h1 className="text-xl md:text-2xl font-semibold">Product Name</h1>
-          <div className="flex items-center gap-x-4 mt-4">
+          <div className="flex items-center gap-x-4 mt-2 sm:mt-4">
             <p className="line-through opacity-70 font-medium">1400৳</p>
             <p className="text-xl font-semibold">750 ৳</p>
           </div>
-          <p className="mt-4 text-gray-600">Product Description</p>
+          <div className="mt-2 sm:mt-4 text-gray-600">
+            <ul className="list-disc ml-4">
+              <li>Feature 1</li>
+              <li>Feature 2</li>
+              <li>Feature 3</li>
+            </ul>
+          </div>
           <div className="flex items-center gap-x-4 text-lg my-2">
             <h1 className="font-semibold">Brand:</h1>
             <p>Boutique</p>
@@ -52,11 +60,16 @@ const ProductDetails = () => {
           <ToggleButton />
 
           <Quantity />
+          <div className="flex items-center gap-x-4 my-4">
+            <AddToCartButton />
+            <BuyNowButton />
+          </div>
         </div>
       </div>
 
       {/* Product Elaborate Description */}
       <div className="mt-4 px-4 py-2 text-lg">
+        <h1 className="font-semibold">Description</h1>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque placeat,
         atque maxime facilis, recusandae minima nihil repudiandae totam error
         molestias reprehenderit ducimus sed! Laboriosam atque dolor dignissimos
--- app/(root)/[category]/[product-name]/ToggleButton.jsx
+++ app/(root)/[category]/[product-name]/ToggleButton.jsx
@@ -1,24 +1,47 @@
+"use client";
 import Button from "@/components/shared/Button";
+import { useState } from "react";
+
+const availableSizes = [
+  {
+    label: "S",
+    value: "sm",
+  },
+  {
+    label: "M",
+    value: "md",
+  },
+  {
+    label: "L",
+    value: "lg",
+  },
+  {
+    label: "XL",
+    value: "xl",
+  },
+  {
+    label: "XXL",
+    value: "xxl",
+  },
+];
 
 const ToggleButton = () => {
+  const [size, setSize] = useState("sm");
+
   return (
     <div className="flex items-center gap-x-4 text-lg my-2">
       <h1 className="font-semibold">Available Sizes:</h1>
-      <Button variant={"outline"} className="rounded-sm py-2">
-        S
-      </Button>
-      <Button variant={"outline"} className="rounded-sm py-2">
-        M
-      </Button>
-      <Button variant={"outline"} className="rounded-sm py-2">
-        L
-      </Button>
-      <Button variant={"outline"} className="rounded-sm py-2">
-        XL
-      </Button>
-      <Button variant={"outline"} className="rounded-sm py-2">
-        XXL
-      </Button>
+
+      {availableSizes.map((item, index) => (
+        <Button
+          variant={size === item.value ? "primary" : "outline"}
+          className="rounded-sm py-2 text-sm"
+          key={index}
+          onClick={() => setSize(item.value)}
+        >
+          {item.label}
+        </Button>
+      ))}
     </div>
   );
 };
